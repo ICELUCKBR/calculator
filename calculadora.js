e75bdd356409cbd0cd88f2086cfa129da436619b
Allan C  Garcia <allancgarcia@outlook.com>
2022-10-20 12:44:40 -0300

    Upload desafio
    Envio das atividades do stage 4 exercicio 1

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+let number1 = prompt(`Digite o primeiro numero`);
+let number2 = prompt(`Digite o segundo numero`);
+
+
+alert("A Soma dos numeros é:  "+(Number(number1)+Number(number2)));
+alert("A Multiplicação dos numeros é:  "+(Number(number1)*Number(number2)));
+alert("A Divisão dos numeros é:  "+(Number(number1)/Number(number2)));
+alert("O Resto da divisão é:  "+(Number(number1)%Number(number2)));
